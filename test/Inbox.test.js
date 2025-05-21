c670b26650859ecab7253af35d0b3e7d1f025738
tom hikari <tomprescott1565@gmail.com>
2025-05-21 16:46:13 +0100

    Enhance Inbox tests by adding contract deployment and message verification. Introduced initial string constant and implemented tests for default and updated messages.

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,20 +3,34 @@ const ganache = require("ganache");
 const { Web3 } = require("web3");
 const { interface, bytecode } = require("../compile");
 // updated ganache and web3 imports added for convenience
-
+const INITIAL_STRING = "Hi there";
 // contract test code will go here
 
 const web3 = new Web3(ganache.provider());
 
-
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-  console.log(accounts);
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
-describe('Inbox', ()=>{
-    it('deploys a contract', ()=>{})
-})
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("bye").send({ from: accounts[0] });
+  });
+});
